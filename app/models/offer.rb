41034bf43bf9cf405baee5aaa2f1a50620e5833b
Cihad Paksoy <cihadp@gmail.com>
2018-05-22 23:54:53 +0300

    bir ilana birden fazla teklif verilemesin.

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,8 +3,15 @@ class Offer < ApplicationRecord
   belongs_to :announcement
   has_many :attachments, as: :attachmentable
   accepts_nested_attributes_for :attachments, allow_destroy: true
+  validate :validate_user_offer_count
 
   def owner? user
     self.user == user
+  end
+
+  def validate_user_offer_count
+    if announcement.offers.exists?(user: user)
+      errors.add(:base, :number_of_offers_exceeded, message: I18n.t('errors.number_of_offers_exceeded'))
+     end
   end
 end
