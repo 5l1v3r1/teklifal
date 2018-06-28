ce8187e8439ef01f30c174e433ee28b0b62a03d2
Cihad Paksoy <cihadp@gmail.com>
2018-06-28 14:03:40 +0300

    araba ilani content controller altina alindi

--- app/policies/car_announcement_policy.rb
+++ app/policies/car_announcement_policy.rb
@@ -1,42 +1,8 @@
-class CarAnnouncementPolicy < ApplicationPolicy
+class CarAnnouncementPolicy < ContentPolicy
 
-  def new?
-    create?
+  private
+
+  def content_attributes
+    @content_attributes ||= [:make]
   end
-
-  def create?
-    signed_user
-  end
-
-  def edit?
-    update?
-  end
-
-  def update?
-    signed_user?(content.announcement) or
-    signed_user.manager?
-  end
-
-  def destroy?
-    signed_user?(content.announcement) or
-    signed_user.manager?
-  end
-
-  def permitted_attributes
-    # Content attributes
-    attrs = [:make]
-    
-    # Add attributes for policy
-    announcement_attrs = { announcement_attributes: [:desc, attachments_attributes: [:id, :file, :_destroy]] }
-    if record.new_record?
-      announcement_attrs[:announcement_attributes] << :duration_day
-    end
-
-    # Add nested attributes to content attributes
-    attrs << node_attrs
-
-    # Return attributes
-    attrs
-  end
-
 end
