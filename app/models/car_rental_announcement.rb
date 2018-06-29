9775582842eaddc31ad0f44b91921dbb5269c319
Cihad Paksoy <cihadp@gmail.com>
2018-06-29 15:39:35 +0300

    teslim zamani alis zamaninda sonra olmasi lazim validationu eklendi

--- app/models/car_rental_announcement.rb
+++ app/models/car_rental_announcement.rb
@@ -8,11 +8,17 @@ class CarRentalAnnouncement < ApplicationRecord
   validates :drop_off_location, geocode: true, presence: true, 
             if: :different_location?
 
+  validate :drop_off_time_must_be_greater_than_pick_up_time
+
   before_validation :empty_drop_off_location!, unless: :different_location?
 
   private
 
   def empty_drop_off_location!
     self.drop_off_location = nil
+  end
+
+  def drop_off_time_must_be_greater_than_pick_up_time
+    errors.add(:drop_off_time, :smaller_than_pick_up_time) if pick_up_time >= drop_off_time
   end
 end
