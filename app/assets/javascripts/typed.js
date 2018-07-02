701ebfc9f0b3b06a946b8870b97216b2cca336fd
Cihad Paksoy <cihadp@gmail.com>
2018-07-02 15:53:38 +0300

    kayan yazi, turbolinks:load dan sonra artik kayiyor

---
+++ app/assets/javascripts/typed.js
@@ -0,0 +1,13 @@
+$(document).on("turbolinks:load", function() {
+  new Typed("#typed", {
+    strings: [
+      "kolay ulaşılabilir olacak.",
+      "daha profesyonel görünecek.",
+      "daha yenilikçi olacak.",
+    ],
+    typeSpeed: 60,
+    loop: true,
+    backSpeed: 25,
+    backDelay: 1500
+  });
+});
