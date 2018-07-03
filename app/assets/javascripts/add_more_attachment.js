d06d6f5518cf76bd4ba04de15920934fd5edbbd9
Cihad Paksoy <cihadp@gmail.com>
2018-07-03 16:35:05 +0300

    daha fazla ek simdi duzenlerken calisiyor

--- app/assets/javascripts/add_more_attachment.js
+++ app/assets/javascripts/add_more_attachment.js
@@ -1,23 +1,20 @@
-$(function() {
-  $(document).on("turbolinks:load", function() {
-    if (!$('.attachment-input').length) return
+$(document).on("turbolinks:load", function() {
+  if (!$('.attachment-input').length) return
 
-    $("input[type=file]").change(function() {
-      var fileInputs = $(this).closest('.form-group').find('input[type=file]')
-
-
-      var isAnEmpty = fileInputs.toArray().some(function(input) {
-        return input.files[0] === undefined
-      })
-
-      console.log(isAnEmpty)
-      if (!!isAnEmpty) return
+  $("input[type=file]").change(function() {
+    var fileInputs = $(this).closest('.form-group').find('input[type=file]')
 
 
-      var clonedField = $(this).closest('.attachment').clone(true)
-      clonedField.find('input[type=file]').val('')
-      $(this).closest('.attachment').parent().append(clonedField)
+    var isAnEmpty = fileInputs.toArray().some(function(input) {
+      return input.files[0] === undefined
     })
-  })
 
-})
+    console.log(isAnEmpty)
+    if (!!isAnEmpty) return
+
+
+    var clonedField = $(this).closest('.attachment').clone(true)
+    clonedField.find('input[type=file]').val('')
+    $(this).closest('.attachment').parent().append(clonedField)
+  })
+});
