9f60ae493eb94ee408c6f89f276b75964441b989
Cihad Paksoy <cihadp@gmail.com>
2018-09-07 15:52:21 +0300

    araba kiralama spotlighttan gelince alis yerine uygun aboneleri listelemiyordu. duzeltildi.

--- app/controllers/car_rental_announcements_controller.rb
+++ app/controllers/car_rental_announcements_controller.rb
@@ -3,6 +3,9 @@ class CarRentalAnnouncementsController < ContentController
   def new
     @subscriptions =  if existing_announcement_for_current_user?
                         CarRentalAnnouncementSubscription.search(@ann.content)
+                      elsif content_params.has_key?(:longitude) and
+                            content_params.has_key?(:latitude)
+                        CarRentalAnnouncementSubscription.search(content_params)
                       else
                         CarRentalAnnouncementSubscription.all
                       end
